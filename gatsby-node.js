dcf275b47f2bf10fd3e8f7a0180eb136f6c20095
Henri Beck <henribeck.dev@gmail.com>
2019-02-15 20:17:11 +0100

    Fix build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,4 @@
 require('ts-node/register/transpile-only');
 const node = require('./src/gatsby/node');
 
-Object.keys(node).forEach((key) => {
-  exports[key] = node[key];
-});
+exports.createPages = node.createPages;
